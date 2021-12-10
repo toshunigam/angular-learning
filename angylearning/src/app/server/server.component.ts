9124a9b25cde87fc0441ecbd23e94d18521b04c9
toshunigam <toshuchandra@gmail.com>
2021-12-10 16:34:22 +0530

    added a compoenet and call them

---
+++ angylearning/src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'server-app',
+    templateUrl:'./server.component.html'
+})
+export class ServerComponent{
+    name = 'Resham'
+}
