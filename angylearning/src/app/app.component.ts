5a8b41938560027174ef8514d3e53759980202df
toshunigam <toshuchandra@gmail.com>
2021-12-13 13:17:57 +0530

    working into directives and passing data thru it

--- angylearning/src/app/app.component.ts
+++ angylearning/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angylearning';
+  serverElements = [{
+    type:'server', name:'Test', content:'Lorem Ipsum'
+  
+  }]
 }
---
+++ angylearning/src/app/server-element/server-element.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+  
+  @Input() element : {type:string,name:string,content:string}
+
+  constructor() {
+    this.element = {type:'',name:'',content:''}
+   }
+
+  ngOnInit(): void {
+  }
+
+}
