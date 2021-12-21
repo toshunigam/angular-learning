8e710acd22524295ea4ef7b9bc1eb118859af4f3
toshunigam <toshuchandra@gmail.com>
2021-12-21 21:14:15 +0530

    example of custom approach to change background color

--- angylearning/src/app/_directive/better-highlight.directive.ts
+++ angylearning/src/app/_directive/better-highlight.directive.ts
@@ -1,12 +1,24 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from "@angular/core";
+import { Directive, 
+    ElementRef, 
+    HostBinding, 
+    HostListener, 
+    Input, 
+    OnInit, 
+    Renderer2 
+} from "@angular/core";
 
 @Directive({
     selector:'[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
-    @HostBinding('style.backgroundColor') backgroundColor:string = 'transparent'
+    @Input() defaultBgColor:string = 'transparent'
+    @Input() highlightBgColor:string = 'blue'
 
-    constructor(private renderer: Renderer2, private eleRef:ElementRef){}
+    @HostBinding('style.backgroundColor') backgroundColor:string;
+
+    constructor(private renderer: Renderer2, private eleRef:ElementRef){
+        this.backgroundColor = this.defaultBgColor
+    }
 
     ngOnInit(): void {
         // this.renderer.setStyle(this.eleRef.nativeElement,'background-color','#ffccaa', false, false)
@@ -14,12 +26,14 @@ export class BetterHighlightDirective implements OnInit{
 
     @HostListener('mouseenter') mouseover(eventData:Event){
         // this.renderer.setStyle(this.eleRef.nativeElement,'background-color','#ffccaa')
-        this.backgroundColor = '#00ccbb'
+        // this.backgroundColor = '#00ccbb'
+        this.backgroundColor = this.highlightBgColor
     }
 
     @HostListener('mouseleave') mouseleave(eventData:Event){
         // this.renderer.setStyle(this.eleRef.nativeElement,'background-color','transparent')
-        this.backgroundColor = 'transparent'
+        // this.backgroundColor = 'transparent'
+        this.backgroundColor = this.defaultBgColor
     }
 
 }
