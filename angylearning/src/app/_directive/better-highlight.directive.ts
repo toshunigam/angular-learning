ca0c7affd8091d7422fbfdaf5feaf874af042510
toshunigam <toshuchandra@gmail.com>
2021-12-21 20:53:30 +0530

    added example of HostListner

--- angylearning/src/app/_directive/better-highlight.directive.ts
+++ angylearning/src/app/_directive/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector:'[appBetterHighlight]'
@@ -7,6 +7,15 @@ export class BetterHighlightDirective implements OnInit{
     constructor(private renderer: Renderer2, private eleRef:ElementRef){}
 
     ngOnInit(): void {
+        // this.renderer.setStyle(this.eleRef.nativeElement,'background-color','#ffccaa')
+    }
+
+    @HostListener('mouseenter') mouseover(eventData:Event){
         this.renderer.setStyle(this.eleRef.nativeElement,'background-color','#ffccaa')
     }
+
+    @HostListener('mouseleave') mouseleave(eventData:Event){
+        this.renderer.setStyle(this.eleRef.nativeElement,'background-color','transparent')
+    }
+
 }
