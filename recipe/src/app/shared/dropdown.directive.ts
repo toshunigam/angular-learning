19dc22de0b81465c1cb3e0a938ba772abdcbddb1
toshunigam <toshuchandra@gmail.com>
2021-12-22 17:06:34 +0530

    working into dropsown directive

--- recipe/src/app/shared/dropdown.directive.ts
+++ recipe/src/app/shared/dropdown.directive.ts
@@ -1,8 +1,14 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from "@angular/core";
 
 @Directive({
     selector:'[appDropdown]'
 })
 export class DropdownDirective{
-    
+    @HostBinding('class.open') isOpen = false
+
+    @HostListener('click') toggleOpen(){
+        this.isOpen = !this.isOpen
+    }
+    constructor(private ele:ElementRef, private renderer:Renderer2){}
+
 }
