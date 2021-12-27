da8b1deb0eae23a4bb56897377a731f8e884abb1
toshunigam <toshuchandra@gmail.com>
2021-12-27 10:28:29 +0530

    working into services

---
+++ recipe/src/app/_services/shoppings.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class ShoppingsService{
+
+    addShoppingList(data:any){
+        console.log(data)
+    }
+}
--- recipe/src/app/shopping-list/shopping-list.component.ts
+++ recipe/src/app/shopping-list/shopping-list.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingsService } from '../_services/shoppings.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingsService]
 })
 
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [];
 
-  constructor() { }
+  constructor(private shoppingService:ShoppingsService) { }
 
   ngOnInit(): void {
+    const ShoppingData = this.shoppingService.addShoppingList({name:'caret',amount:12})
+    console.log(ShoppingData)
   }
+
 
   createdShoppingList(shoppingData:{name:string,amount:number}){
     this.ingredients.push(new Ingredient(shoppingData.name,shoppingData.amount))
