f8018aae9a7b25885449e8100c05d02e46b315fd
toshunigam <toshuchandra@gmail.com>
2021-12-30 15:59:25 +0530

    practice on product as per shopping list

--- recipe/src/app/app.module.ts
+++ recipe/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { FormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
