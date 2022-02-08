369b4d10c6ec89de64d09a7d15e871beb2e9bd96
toshunigam <toshuchandra@gmail.com>
2022-02-08 20:30:13 +0530

    transforming output using pipes

--- recipe/src/app/servers/servers.component.ts
+++ recipe/src/app/servers/servers.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  servers = [
+    {
+      instanceType:'medium',
+      name:'Production Server',
+      status:'stable',
+      started: new Date(1,1,2022)
+    },
+    {
+      instanceType:'Large',
+      name:'User Database',
+      status:'stable',
+      started: new Date(2,2,2022)
+    },
+    {
+      instanceType:'small',
+      name:'Dev Server',
+      status:'offline',
+      started: new Date(1,12,2020)
+    },
+    {
+      instanceType:'small',
+      name:'Testing Server',
+      status:'stable',
+      started: new Date(1,12,2022)
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getStatusClasses(server:{instanceType:string,name:string,status:string,started:Date}){
+    return {
+      'list-group-item-success':server.status === 'stable',
+      'list-group-item-warning':server.status === 'offline',
+      'list-group-item-dabger':server.status === 'critical'
+    }
   }
 
 }
